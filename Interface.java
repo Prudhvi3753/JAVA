0361cdda204332297e3f5ca48e87bb418aeb4384
Prudhvi3753 <prudhvich45@gmail.com>
2025-06-19 10:04:12 +0530

    progress

--- Interface.java
+++ Interface.java
@@ -1,6 +1,9 @@
-interface Test1{
-    void meth1();
-    void meth2();
+interface Test{
+    public abstract void meth1();
+    public abstract void meth2();
+}
+interface Test1 extends Test{
+    public abstract void meth3();
 }
 class Test2 implements Test1{
     public void meth1(){
@@ -9,13 +12,14 @@ class Test2 implements Test1{
     public void meth2(){
         System.out.println("method 2");
     }
-    
+    public void meth3(){
+        System.out.println("method 3");
+    }
 }
-public class Interface {
-    public static void main(String[] args) {
-        Test1 t=new Test2();
-        t.meth1();
+
+public class Interface{
+    public static void main(String[] args){
+        Test2 t=new Test2();
         t.meth2();
-        
     }
 }
